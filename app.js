5a8849738666884fff3868ae748987d1dcefcfb2
TharinduLala <tharindulalantha2001@gmail.com>
2024-12-17 23:44:46 +0530

    updt

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/uploads', express.static('uploads'));
-app.use("/api", (req,res)=>{
+app.use("/", (req,res)=>{
   res.send("Hello Advisa")
 });
 app.use("/api/login", loginRoutes);
